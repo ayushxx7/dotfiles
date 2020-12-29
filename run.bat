5374b918bb385e9302c63e44602bb5d7b2442136
Ayush Mandowara <ayushxx7@gmail.com>
2020-12-29 16:35:03 +0530

    feat(one-click-setup): auto-launch bat in Admin

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 echo "Copying .vimrc to:" %userprofile%
 cp .vimrc %userprofile%
 echo "Removing _vimrc:" from %userprofile%\_vimrc
