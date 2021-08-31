71a2ce5c6ead99f8eed6004b5a0fcd10be3a2fed
Ayush Mandowara <ayushxx7@gmail.com>
2021-08-31 12:58:45 +0530

    fix(conf): use windows commands for copy, move

--- run.bat
+++ run.bat
@@ -3,12 +3,12 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 echo "Removing existing .vimrc:" from %userprofile%\.vimrc
 echo "Storing removed .vimrc as .vimrc_old"
-mv %userprofile%\.vimrc %userprofile%\.vimrc_old
+move %userprofile%\.vimrc %userprofile%\.vimrc_old
 echo "Removing existing _vimrc:" from %userprofile%\_vimrc
 echo "Storing removed _vimrc as _vimrc_old"
-mv %userprofile%\_vimrc %userprofile%\_vimrc_old
+move %userprofile%\_vimrc %userprofile%\_vimrc_old
 echo "Copying setup .vimrc to:" %userprofile%
-cp .vimrc %userprofile%
+copy .vimrc %userprofile%
 echo "Copying vim directory to .config folder"
 mkdir %userprofile%\.config
 robocopy vim %userprofile%\.config\vim /E
@@ -19,9 +19,9 @@ mkdir %userprofile%\.vim\undo-dir
 echo "Creating autoload directory for vim plug - Plugin Manager"
 mkdir %userprofile%\vimfiles\autoload
 echo "Copying plug.vim to autoload directory so it loads as vim loads"
-cp plug.vim %userprofile%\vimfiles\autoload\
+copy plug.vim %userprofile%\vimfiles\autoload\
 echo "Copying coc-settings.json to autoload directory so it loads as vim loads. it contains some coc prefs"
-cp coc-settings.json %userprofile%\vimfiles\autoload\
+copy coc-settings.json %userprofile%\vimfiles\autoload\
 echo "Installing Chocolatey Package Manager"
 Powershell.exe -executionpolicy bypass -File choco_install_powershell.ps1
 echo "Installing Node"
